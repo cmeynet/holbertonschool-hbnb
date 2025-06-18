870742b6359fe941539181d8c8f1f780255f5c6d
Victoire07 <victoireboutin07@gmail.com>
2025-06-18 19:20:43 +0200

    Creation class

--- part2/app/tests/test_review.py
+++ part2/app/tests/test_review.py
@@ -5,3 +5,13 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 import unittest
+from app.models.review import Review
+from app.models.user import User
+from app.models.place import Place
+
+class TestReviewModel(unittest.TestCase):
+    def setUp(self):
+        self.user = User("Clemence", "Meynet", "clemence@exemple.com")
+        self.place = Place("Studio", 100.0, 45.0, 1.0, self.user)
+
+
