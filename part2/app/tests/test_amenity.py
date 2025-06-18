d6af0d27efedc25be4d0cff32e51483d719bc4b4
Victoire07 <victoireboutin07@gmail.com>
2025-06-18 17:05:42 +0200

    Pycodestyle modification

--- part2/app/tests/test_amenity.py
+++ part2/app/tests/test_amenity.py
@@ -24,21 +24,3 @@ class TestAmenityModel(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 import unittest
-from app.models.amenity import Amenity
-
-class TestAmenityModel(unittest.TestCase):
-
-    def test_valid_amenity(self):
-        a = Amenity("Jacuzzi")
-        self.assertEqual(a.name, "Jacuzzi")
-
-    def test_invalid_name_type(self):
-        with self.assertRaises(TypeError):
-            Amenity(123)
-
-    def test_name_too_long(self):
-        with self.assertRaises(ValueError):
-            Amenity("x" * 100)
-
-if __name__ == "__main__":
-    unittest.main()
