d68b07c3da5058f6d5a5cdd01d72c8d49c393cc4
Victoire07 <victoireboutin07@gmail.com>
2025-06-20 09:35:57 +0200

    Save modification

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -13,9 +13,5 @@ def create_app():
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
-<<<<<<< HEAD
-    api.add_namespace(places_ns, path="/api/v1/reviews")
-=======
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
->>>>>>> dfb29ad2b4286cf3bb2e79e70eeebb2261cc4683
     return app
--- part2/app/tests/test_amenity.py
+++ part2/app/tests/test_amenity.py
@@ -3,7 +3,6 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
 import unittest
 from app.models.amenity import Amenity
 
