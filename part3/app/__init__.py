a18aa230bdc659165c76be8596b7cbaa9a935fca
Victoire07 <victoireboutin07@gmail.com>
2025-07-01 11:07:28 +0200

    improved Flask factory with config

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -5,9 +5,27 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
+    """
+    Flask application factory that instantiates the app with a configuration class
+    """
     app = Flask(__name__)
+
+    # Étape n°1 : Charger la configuration de l'application avant d'initialiser les extensions(BD...)
+    app.config.from_object(config_class)
+
+    # Étape n°2 :   Initialiser ici les outils utilisés par l'application, comme la BD ou l'authentification (JWT...)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+
+    # Étape n°3 :  Créer l’API RestX qui permettra de déclarer les routes & générer la doc automatique
+    api = Api(
+        app,
+        version="1.0",
+        title="HBnB API",
+        description="HBnB Application API"
+    )
+
+    # Étape n°4 : Enregistrer les namespaces pour activer les routes de chaque fonctionnalité (users, places, reviews, etc.) 
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
