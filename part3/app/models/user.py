14e7c37f9eec581bfecfa11e956752376f83c1f3
Victoire07 <victoireboutin07@gmail.com>
2025-07-02 11:44:19 +0200

    add password parameter to __init__ (constructor)

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -5,7 +5,7 @@ import re
 class User(BaseModel):
     emails = set()
 
-    def __init__(self, first_name, last_name, email, is_admin=False):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
