8e465fdb02ea4e3d8bbedea7db99c4e385d026e1
Victoire07 <victoireboutin07@gmail.com>
2025-07-03 16:10:14 +0200

    register auth namespace for JWT login endpoint

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -9,6 +9,7 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.protected import api as protected_ns
+from app.api.v1.auth import api as auth_ns
 
 jwt = JWTManager()
 
@@ -40,4 +41,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(protected_ns, path='/api/v1')
+    api.add_namespace(auth_ns,      path="/api/v1/auth")
     return app
