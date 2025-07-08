a369db66a3a726cf9ff828e27599ba9800913915
Victoire07 <victoireboutin07@gmail.com>
2025-07-08 14:04:55 +0200

    pycodestyle and comments

--- part3/app/extensions.py
+++ part3/app/extensions.py
@@ -4,12 +4,14 @@ from flask_jwt_extended import verify_jwt_in_request, get_jwt
 from flask import jsonify
 bcrypt = Bcrypt()
 
+
 def admin_required(fn):
     """
     Authorizes only JWT requests with is_admin=True
     """
     @wraps(fn)
-    def wrapper(*args, **kwargs): # * : args wth nme et ** wth
+    def wrapper(*args, **kwargs):
+        # * : args wth nme et ** wth
         verify_jwt_in_request()
         claims = get_jwt()
         if not claims.get("is_admin", False):
