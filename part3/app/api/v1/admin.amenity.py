d7b3adbe6d4bcc9cfa4a6141a6e3cdfc96e1c0c7
Victoire07 <victoireboutin07@gmail.com>
2025-07-08 16:05:49 +0200

    add admin-only routes for creating and updating amenities (task5)

--- part3/app/api/v1/admin.amenity.py
+++ part3/app/api/v1/admin.amenity.py
@@ -10,3 +10,32 @@ amenity_in = api.model("AdminAmenityIn", {
 amenity_out = api.clone("AdminAmenityOut", amenity_in, {
     "id": fields.String
 })
+
+@api.route("/")
+class AdminAmenityList(Resource):
+    @admin_required
+    @api.expect(amenity_in, validate=True)
+    @api.marshal_with(amenity_out, code=201)
+    def post(self):
+        """
+        Add a new amenity (admin only not all users)
+        """
+        data = api.payload
+        amenity = facade.create_amenity(data)
+        return amenity.to_dict(), 201
+
+
+@api.route("/<amenity_id>")
+class AdminAmenityResource(Resource):
+    @admin_required
+    @api.expect(amenity_in, validate=True)
+    @api.marshal_with(amenity_out)
+    @api.response(404, "Amenity not found")
+    def put(self, amenity_id):
+        """
+        Modify an amenity (admin only not all users!!)"""
+        if not facade.get_amenity(amenity_id):
+            return {"error": "Amenity not found"}, 404
+
+        updated = facade.update_amenity(amenity_id, api.payload)
+        return updated.to_dict()
