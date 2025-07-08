13f5968beae78a47638e88bd99021b5ec5748d5e
Victoire07 <victoireboutin07@gmail.com>
2025-07-08 14:41:02 +0200

    add namespace to activate route for user_admin

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -44,4 +44,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(protected_ns, path='/api/v1')
     api.add_namespace(auth_ns, path="/api/v1/auth")
+    api.add_namespace(admin_users_ns, path='/api/v1/admin/users')
     return app
