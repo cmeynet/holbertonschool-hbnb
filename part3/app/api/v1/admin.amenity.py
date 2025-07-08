86aff65bf76296fe453957a263f46f07f5394bc6
Victoire07 <victoireboutin07@gmail.com>
2025-07-08 16:03:34 +0200

    add model for requests PUT/POST

--- part3/app/api/v1/admin.amenity.py
+++ part3/app/api/v1/admin.amenity.py
@@ -1,3 +1,12 @@
 from flask_restx import Namespace, Resource, fields
 from app.extensions import admin_required
 from app.services import facade
+
+api = Namespace("admin_amenities", description="Admin: manage amenities")
+
+amenity_in = api.model("AdminAmenityIn", {
+    "name": fields.String(required=True, description="Amenity name")
+})
+amenity_out = api.clone("AdminAmenityOut", amenity_in, {
+    "id": fields.String
+})
