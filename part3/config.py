806615fd80114ab20341497c48beda62a97ba088
Victoire07 <victoireboutin07@gmail.com>
2025-07-08 19:51:29 +0200

    pycodestyle fonc config.py

--- part3/config.py
+++ part3/config.py
@@ -1,12 +1,15 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'dev-jwt-secret')
+
 
 config = {
     'development': DevelopmentConfig,
