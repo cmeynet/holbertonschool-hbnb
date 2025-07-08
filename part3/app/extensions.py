56fdd433626636c5e9c463da5d24db30b303f17b
Victoire07 <victoireboutin07@gmail.com>
2025-07-08 14:02:25 +0200

    add admin_required to extensions.py for admin route checks

--- part3/app/extensions.py
+++ part3/app/extensions.py
@@ -3,3 +3,16 @@ from functools import wraps
 from flask_jwt_extended import verify_jwt_in_request, get_jwt
 from flask import jsonify
 bcrypt = Bcrypt()
+
+def admin_required(fn):
+    """
+    Authorizes only JWT requests with is_admin=True
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs): # * : args wth nme et ** wth
+        verify_jwt_in_request()
+        claims = get_jwt()
+        if not claims.get("is_admin", False):
+            return jsonify({"error": "Admin privileges required"}), 403
+        return fn(*args, **kwargs)
+    return wrapper
