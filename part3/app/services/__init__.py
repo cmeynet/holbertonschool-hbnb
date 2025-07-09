1aebc2b72b44a51bd28946ad6b9825be99209738
Victoire07 <victoireboutin07@gmail.com>
2025-07-09 15:44:20 +0200

    add admin for start API

--- part3/app/services/__init__.py
+++ part3/app/services/__init__.py
@@ -1,3 +1,6 @@
 from .facade import HBnBFacade
 
 facade = HBnBFacade()
+
+# Création d'un admin à chaque lancement de l'API
+facade.create_user({'first_name': 'Vivi', 'last_name': 'Boubou', 'email': 'admin@gmail.com', 'password': 'admin', 'is_admin': True})
